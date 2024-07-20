acd7327178dcbb741c2f6a3ee58ad2fcd2638fbc
Joe Turco <jaturco00@gmail.com>
2024-07-20 17:28:26 -0400

    dust effects good, more art on screen

--- Scripts/EnvironmentManager.gd
+++ Scripts/EnvironmentManager.gd
@@ -5,6 +5,8 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+
+
 var vol_slider
 var music_manager
 var blueshift
@@ -12,6 +14,7 @@ var redshift
 
 var pulser
 
+var basedust = load("res://Scenes/dustevent.tscn")
 
 var aberration = 0
 var ab_rate = 0.01
@@ -23,7 +26,6 @@ func _ready():
 	blueshift = get_node("Aberration Wall/Blue Shift")
 	redshift = get_node("Aberration Wall/Red Shift")
 	pulser = get_node("Main Wall")
-	#begin_aberration()
 	pass # Replace with function body.
 	
 func pulse_effect(position):
@@ -31,6 +33,24 @@ func pulse_effect(position):
 	pass
 	
 func spawn_particles(position, direction):
+	var d = basedust.instance()
+	d.position = position
+	d.quantity = 6
+	d.dust_color = pulser.cur_color
+	if direction.x < 0:
+		d.x_min_rate = -1
+		d.x_max_rate = -0.25
+	else:
+		d.x_min_rate = 0.25
+		d.x_max_rate = 1	
+	
+	if direction.y < 0:
+		d.y_min_rate = -1
+		d.y_max_rate = -0.25
+	else:
+		d.y_min_rate = 0.25
+		d.y_max_rate = 1	
+	add_child(d)
 	pass
 	
 
@@ -51,16 +71,17 @@ func _process(delta):
 		redshift.position.y -= 0.2
 		if blueshift.modulate.a >= 0.2:
 			aberration = 0
-			backpedal_aberration()
+			#backpedal_aberration()
 	elif aberration == -1:
 		blueshift.modulate.a -= 0.01
 		blueshift.position.x -= 0.2
 		blueshift.position.y -= 0.1
+		
 		redshift.position.x += 0.2
 		redshift.position.y += 0.1
 		redshift.modulate.a -= 0.01
 		if blueshift.modulate.a <= 0:
 			aberration = 0
-			begin_aberration()
+			#begin_aberration()
 	music_manager.master_max_gain = vol_slider.value # Slider
 	pass
--- Scripts/TileEffect.gd
+++ Scripts/TileEffect.gd
@@ -2,17 +2,19 @@ extends TileMap
 
 var pulseeffect = load("res://Scenes/pulseeffecttiles.tscn")
 #var dusteffect = 
-export var red_shift_rate = 0.0005
+export var rate = 0.0005
 export var starting_color = Color(0.75,0.75,1,1)
+export var ending_color = Color(1,1,1,1)
 export var game_started = 0
 
 var at_maximum = 0
 
-var cur_color = starting_color
+export var cur_color = Color(0.75,0.75,1,1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	cur_color = starting_color
+	game_started = 1
 	#pulse_tiles(Vector2(1,1))
 	pass # Replace with function body.
 	
@@ -39,9 +41,16 @@ func spawn_particles(position, direction):
 # Red shifts the default pulse color every frame
 func _process(delta):
 	if game_started != 0:
-		if at_maximum == 0:
-			cur_color.b -= red_shift_rate
-			cur_color.r += red_shift_rate
-			if cur_color.r >= 1:
-				at_maximum = 1
+		if ending_color.r > cur_color.r:
+			cur_color.r += rate
+		elif ending_color.r <= cur_color.r:
+			cur_color.r -= rate
+		if ending_color.b > cur_color.b:
+			cur_color.b += rate
+		elif ending_color.b <= cur_color.b:
+			cur_color.b -= rate
+		if ending_color.g > cur_color.g:
+			cur_color.g += rate
+		elif ending_color.g <= cur_color.g:
+			cur_color.g -= rate
 	pass
