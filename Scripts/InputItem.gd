55e7ec349010d02ea71e53644253bb65e89db6d9
Sebastian Kobori <sjkobori@gmail.com>
2024-07-21 00:39:38 -0400

    Formatted combo UI

--- Scripts/InputItem.gd
+++ Scripts/InputItem.gd
@@ -4,6 +4,9 @@ extends Label
 var input_val : String setget set_input_val
 var pressed : bool setget set_pressed
 
+func _ready():
+	modulate = Color("#b3b0ff")
+
 # Change set methods if using buttons instead of labels
 func set_input_val(val : String):
 	input_val = val
@@ -12,7 +15,7 @@ func set_input_val(val : String):
 func set_pressed(val : bool):
 	pressed = val
 	if val:
-		modulate = Color.red
+		modulate = Color("#e7f37f")
 	else:
-		modulate = Color.white
+		modulate = Color("#b3b0ff")
 
